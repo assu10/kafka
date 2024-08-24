f298f56689c8b8f03b5414dc46f9ae7d6d2642eb
assu10 <juhyun10.lee@gmail.com>
2024-08-24 19:19:09 +0900

    1.3. 컨슈머 그룹 수정: `alterConsumerGroupOffsets()`

--- chap05/src/main/java/com/assu/study/chap05/AdminClientSample2.java
+++ chap05/src/main/java/com/assu/study/chap05/AdminClientSample2.java
@@ -12,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.*;
 import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.errors.UnknownMemberIdException;
 
 @Slf4j
 public class AdminClientSample2 {
@@ -91,6 +92,46 @@ public class AdminClientSample2 {
           (latestOffset - committedOffset));
       // 아래는 확실하지 않음
       // log.info("The earliest offset: {}, order offset: {}", earliestOffset, orderOffset);
+    }
+
+    // ======= 컨슈머 그룹을 특정 오프셋으로 리셋
+
+    // 1)
+    // 맨 앞 오프셋부터 처리하도록 컨슈머 그룹을 리셋하기 위해 토픽의 맨 앞 오프셋 값 조회
+    // (reqOlderOffsets 를 사용하면 2시간 전으로 오프셋 리셋 가능)
+    Map<TopicPartition, ListOffsetsResult.ListOffsetsResultInfo> earliestOffsets =
+        adminClient.listOffsets(reqEarliestOffsets).all().get();
+
+    Map<TopicPartition, OffsetAndMetadata> resetOffsets = new HashMap<>();
+
+    // 2)
+    // 반복문을 이용하여 listOffsets() 가 리턴한 ListOffsetsResultInfo 의 맵 객체를 alterConsumerGroupOffsets() 를
+    // 호출하는데 필요한 OffsetAndMetadata 의 맵 객체로 변환
+    for (Map.Entry<TopicPartition, ListOffsetsResult.ListOffsetsResultInfo> e :
+        earliestOffsets.entrySet()) {
+      log.info("Will reset topic-partition: {}, to offset: {}", e.getKey(), e.getValue().offset());
+      resetOffsets.put(e.getKey(), new OffsetAndMetadata(e.getValue().offset()));
+    }
+
+    try {
+      // 3)
+      //  alterConsumerGroupOffsets() 를 호출한 뒤 get() 을 호출하여 Future 객체가 작업을 성공적으로 완료할 때까지 기다림
+      adminClient.alterConsumerGroupOffsets(CONSUMER_GROUP, resetOffsets).all().get();
+    } catch (ExecutionException e) {
+      log.error(
+          "Failed to update the offsets committed by group: {}, with error: {}",
+          CONSUMER_GROUP,
+          e.getMessage());
+
+      // 4)
+      // alterConsumerGroupOffsets() 가 실패하는 가장 흔한 이유 중 하나는 컨슈머 그룹을 미리 정지시키지 않아서임
+      // 특정 컨슈머 그룹을 정지시키는 어드민 명령은 없기 때문에 컨슈머 애플리케이션을 정지시키는 것 외에는 방법이 없음
+      // 만일 컨슈머 그룹이 여전히 돌아가고 있는 중이라면, 컨슈머 코디네이터 입장에서는 컨슈머 그룹에 대한 오프셋 변경 시도를 그룹의 멤버가 아닌
+      // 클라이언트가 오프셋을 커밋하려는 것으로 간주함
+      // 이 경우 UnknownMemberIdException 이 발생됨
+      if (e.getCause() instanceof UnknownMemberIdException) {
+        log.error("Check if consumer group is still active.");
+      }
     }
   }
 }
