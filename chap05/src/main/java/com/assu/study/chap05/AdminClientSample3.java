4b4827cd88715c2bb946cdeaa370eae88de64507
assu10 <juhyun10.lee@gmail.com>
2024-08-25 15:21:14 +0900

    3.4. replica 재할당: `alterPartitionReassignments()`

--- chap05/src/main/java/com/assu/study/chap05/AdminClientSample3.java
+++ chap05/src/main/java/com/assu/study/chap05/AdminClientSample3.java
@@ -1,5 +1,6 @@
 package com.assu.study.chap05;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -12,12 +13,14 @@ import org.apache.kafka.common.ElectionType;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.errors.ElectionNotNeededException;
 import org.apache.kafka.common.errors.InvalidPartitionsException;
+import org.apache.kafka.common.errors.NoReassignmentInProgressException;
 
 @Slf4j
 public class AdminClientSample3 {
   public static final String CONSUMER_GROUP = "testConsumerGroup";
   public static final String TOPIC_NAME = "sample-topic";
   public static final int NUM_PARTITIONS = 6;
+  private static final List<String> TOPIC_LIST = List.of(TOPIC_NAME);
 
   public static void main(String[] args) throws ExecutionException, InterruptedException {
     Properties props = new Properties();
@@ -80,5 +83,61 @@ public class AdminClientSample3 {
         log.error("All leaders are preferred leaders, no need to do anything.");
       }
     }
+
+    // ======= 새로운 브로커로 파티션 재할당 (replica 재할당)
+    Map<TopicPartition, Optional<NewPartitionReassignment>> reassignment = new HashMap<>();
+
+    // 1)
+    // 파티션 0 에 새로운 replica 를 추가하고, 새 replica 를 ID 가 1인 새 브로커에 배치
+    // 단, 리더는 변경하지 않음
+    reassignment.put(
+        new TopicPartition(TOPIC_NAME, 0),
+        Optional.of(new NewPartitionReassignment(Arrays.asList(0, 1))));
+
+    // 2)
+    // 파티션 1 에는 아무런 replica 도 추가하지 않음
+    // 단지 이미 있던 replica 를 새 브로커로 옮겼을 뿐임
+    // replica 가 하나뿐인만큼 이것이 리더가 됨
+    reassignment.put(
+        new TopicPartition(TOPIC_NAME, 1),
+        Optional.of(new NewPartitionReassignment(Arrays.asList(0))));
+
+    // 3)
+    // 파티션 2 에 새로운 replica 를 추가하고 이것을 선호 리더로 설정
+    // 다음 선호 리더 선출 시 새로운 브로커에 있는 새로운 replica 로 리더가 바뀌게 됨
+    // 이전 replica 는 팔로워가 될 것임
+    reassignment.put(
+        new TopicPartition(TOPIC_NAME, 2),
+        Optional.of(new NewPartitionReassignment(Arrays.asList(1, 0))));
+
+    // 4)
+    // 파티션 3 에 대해서는 진행중인 재할당 작업이 업음
+    // 하지만 그런게 있다면 작업이 취소되고 재할당 작업이 시작되기 전 상태로 원상복구될 것임
+    reassignment.put(new TopicPartition(TOPIC_NAME, 3), Optional.empty());
+
+    try {
+      adminClient.alterPartitionReassignments(reassignment).all().get();
+    } catch (ExecutionException e) {
+      if (e.getCause() instanceof NoReassignmentInProgressException) {
+        log.error(
+            "We tried cancelling a reassignment that was not happening anyway. Let's ignore this.");
+      }
+    }
+
+    // 5)
+    // 현재 진행중인 재할당을 보여줌
+    log.info(
+        "Currently reassigning: {}",
+        adminClient.listPartitionReassignments().reassignments().get());
+
+    DescribeTopicsResult sampleTopic = adminClient.describeTopics(TOPIC_LIST);
+    TopicDescription topicDescription = sampleTopic.topicNameValues().get(TOPIC_NAME).get();
+
+    // 6)
+    // 새로운 상태를 보여줌
+    // 단, 일관적인 결과가 보일 때까지는 잠시 시간이 걸릴 수 있음
+    log.info("Description of sample topic: {}", topicDescription);
+
+    adminClient.close(Duration.ofSeconds(30));
   }
 }
