0e0b2f3cb44790bd2473762f74a2f3376688fbd2
assu10 <juhyun10.lee@gmail.com>
2024-08-25 13:42:28 +0900

    3.2. 토픽에서 레코드 삭제: `deleteRecords()`

--- chap05/src/main/java/com/assu/study/chap05/AdminClientSample3.java
+++ chap05/src/main/java/com/assu/study/chap05/AdminClientSample3.java
@@ -1,17 +1,21 @@
 package com.assu.study.chap05;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.NewPartitions;
+import org.apache.kafka.clients.admin.*;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.errors.InvalidPartitionsException;
 
 @Slf4j
 public class AdminClientSample3 {
+  public static final String CONSUMER_GROUP = "testConsumerGroup";
   public static final String TOPIC_NAME = "sample-topic";
   public static final int NUM_PARTITIONS = 6;
 
@@ -35,5 +39,27 @@ public class AdminClientSample3 {
         log.error("Couldn't modify number of partitions in topic: {}", e.getMessage());
       }
     }
+
+    // ======= 토픽에서 레코드 삭제
+    Map<TopicPartition, OffsetAndMetadata> offsets =
+        adminClient.listConsumerGroupOffsets(CONSUMER_GROUP).partitionsToOffsetAndMetadata().get();
+
+    Map<TopicPartition, OffsetSpec> reqOrderOffsets = new HashMap<>();
+    Instant resetTo = ZonedDateTime.now(ZoneId.of("Asia/Seoul")).minusHours(2).toInstant();
+
+    for (TopicPartition tp : offsets.keySet()) {
+      reqOrderOffsets.put(tp, OffsetSpec.forTimestamp(resetTo.toEpochMilli()));
+    }
+
+    Map<TopicPartition, ListOffsetsResult.ListOffsetsResultInfo> olderOffsets =
+        adminClient.listOffsets(reqOrderOffsets).all().get();
+
+    Map<TopicPartition, RecordsToDelete> recordsToDelete = new HashMap<>();
+    for (Map.Entry<TopicPartition, ListOffsetsResult.ListOffsetsResultInfo> e :
+        olderOffsets.entrySet()) {
+      recordsToDelete.put(e.getKey(), RecordsToDelete.beforeOffset(e.getValue().offset()));
+    }
+    
+    adminClient.deleteRecords(recordsToDelete).all().get();
   }
 }
